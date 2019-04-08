9a9af4a971832ed25161d0372324fae496244ac6
Nick Liu <xuewei.liu@ge.com>
2019-04-08 20:04:01 +0800

    try camel with springboot

---
+++ datacollector/src/main/java/cn/com/gehc/cpm/datacollector/config/DataSourceConfig.java
@@ -0,0 +1,29 @@
+package cn.com.gehc.cpm.datacollector.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+@ImportResource(locations = {"beans/*.xml"})
+
+    @Autowired
+    private Environment env;
+
+    @Bean("defaultDS")
+    @Primary
+    public DataSource primaryDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(env.getProperty("custom.datasource.primary.driverClassName"));
+        dataSource.setUrl(env.getProperty("custom.datasource.primary.url"));
+        dataSource.setUsername(env.getProperty("custom.datasource.primary.username"));
+        dataSource.setPassword(env.getProperty("custom.datasource.primary.password"));
+        return dataSource;
+    }
+}
