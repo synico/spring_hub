f7958f1bedcf9cc836ffdd00afe7c3b720d1ced8
Nick Liu <xuewei.liu@ge.com>
2019-04-29 15:09:46 +0800

    verify timer

--- datacollector/src/main/java/cn/com/gehc/cpm/datacollector/jobs/PullDataJob1.java
+++ datacollector/src/main/java/cn/com/gehc/cpm/datacollector/jobs/PullDataJob1.java
@@ -1,5 +1,7 @@
 package cn.com.gehc.cpm.datacollector.jobs;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.time.Instant;
@@ -9,18 +11,20 @@ import java.time.format.DateTimeFormatter;
 @Component
 public class PullDataJob1 {
 
+    private static final Logger log = LoggerFactory.getLogger(PullDataJob1.class);
+
     private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd hh:mm:ss");
 
     public void pullData() {
         LocalDateTime now = LocalDateTime.now();
-        System.out.println("now: " + now.format(formatter));
+        log.info("pullData1 start date: {}", now.format(formatter));
         try {
-            Thread.sleep(10000);
+            Thread.sleep(3000);
         } catch (Exception ex) {
             ex.printStackTrace();
         }
         now = LocalDateTime.now();
-        System.out.println("pullData1: " + now.format(formatter));
+        log.info("pullData1 end date: {}", now.format(formatter));
     }
 
 }
--- datacollector/src/main/java/cn/com/gehc/cpm/datacollector/jobs/PullDataJob2.java
+++ datacollector/src/main/java/cn/com/gehc/cpm/datacollector/jobs/PullDataJob2.java
@@ -1,5 +1,7 @@
 package cn.com.gehc.cpm.datacollector.jobs;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
@@ -8,11 +10,13 @@ import java.time.format.DateTimeFormatter;
 @Component
 public class PullDataJob2 {
 
+    private static final Logger log = LoggerFactory.getLogger(PullDataJob2.class);
+
     private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd hh:mm:ss");
 
     public void pullData() {
         LocalDateTime now = LocalDateTime.now();
-        System.out.println("pullData2: " + now.format(formatter));
+        log.info("pullData2 date: {}", now.format(formatter));
     }
 
 }
